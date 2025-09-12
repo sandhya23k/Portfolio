d9c5e8eaebc1e26c5f63a76834a7e1ecc567e219
sandhya23k <sandhyakoganti123@gmail.com>
2025-09-12 14:13:54 -0400

    New Changes

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,11 +9,14 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Sandhya Koganti </span>
-            from United States<span className="purple"> .</span>
-            <br />
-            I am currently employed as a Associate IT Consultant at ITC Infotech.
-            <br />
-            I am holding a Master's Degree in Computer Science Engineering at USF.
+            a software engineer with a passion for building innovative applications in robotics, AI, and web development. I hold a Master’s in Computer Science from the University of South Florida, and I have 3+ years of professional experience developing full-stack solutions using C#, .NET, React.js, and Python.
+          <span className="purple"> .</span>
+          <br />
+          <br />
+          I enjoy turning complex problems into intuitive solutions, whether it’s designing automated trading systems, creating interactive web applications, or developing AI models . I thrive in collaborative environments and love learning new technologies to stay ahead in the ever-evolving tech landscape.
+          <br />
+          <br />
+          When I’m not coding, I enjoy exploring emerging tech in robotics, AI, and software solutions, and sharing knowledge through personal projects and contributions to open-source communities.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
