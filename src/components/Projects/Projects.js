ad72b9a01b954871e8e14812ed06165dc94074a1
sandhya23k <sandhyakoganti123@gmail.com>
2025-09-12 12:24:10 -0400

    New Changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,8 +4,8 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 //import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+
+import ExpenseCalculator from "../../Assets/Projects/ExpenseCalculator.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
@@ -23,12 +23,11 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={ExpenseCalculator}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-             
+              title="Expense Calculator"
+              description="Expense Calculator to Add/Delete/Edit Expenses, to keep track of montly expenses of an Individual. Built using .Net MVC, Entity Framework."
+              ghLink="https://github.com/sandhya23k/ExpenseCalculator"
             />
           </Col>
 
@@ -45,12 +44,12 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={leaf}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="AI Knowledge Retreival"
+              description="This project implements task planning for robotic cooking using a Functional Object-Oriented Network (FOON).  It generates sequences of actions (task trees) to achieve a cooking goal based on the current kitchen state."
+              ghLink="https://github.com/sandhya23k/AI--Knowledge-Retrieval"
+                           
             />
           </Col>
 
